8d62b3252efcfdd35a7298ec267489329c383d26
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-04-28 22:44:24 +0500

    Дамп БД

---
+++ public.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local-postgre
+ Source Server Type    : PostgreSQL
+ Source Server Version : 110002
+ Source Host           : localhost:5432
+ Source Catalog        : films_spoilers
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 110002
+ File Encoding         : 65001
+
+ Date: 28/04/2019 22:43:44
+*/
+
